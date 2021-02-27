5fe48c2209cfab8162f0f5a3789289c0e700d05f
davidding2020 <david_ding99@yahoo.com>
2021-02-27 12:16:03 -0500

    changes after first push to remote

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("modified  Hello, world from GIT!");
       System.out.println("added2  Hello, world from GIT!");
+      System.out.println("change after first push to remote,  Hello, world from GIT!");
    }
 }
