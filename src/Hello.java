c89e49efc567efc21159bbb52a2717f46f48a5cf
davidding2020 <david_ding99@yahoo.com>
2021-02-27 11:33:23 -0500

    2 changes2 changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@
 // Hello.java
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
+      System.out.println("modified  Hello, world from GIT!");
+      System.out.println("added2  Hello, world from GIT!");
    }
 }
