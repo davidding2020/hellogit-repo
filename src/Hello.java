f0b25554e91123d2b981c62aa7777e1726314b66
davidding2020 <david_ding99@yahoo.com>
2021-02-28 19:04:59 -0500

    add in master before merge

--- src/Hello.java
+++ src/Hello.java
@@ -11,6 +11,7 @@ public class Hello {
       System.out.println("change after other clone, user1 update,  Hello, world from GIT!");
 System.out.println("change 1");
 System.out.println("change 2");
+System.out.println("merge test, add to master");
       
 >>>>>>> ea28694c0deb0a300d026b82d1a2172bf0739a7f
    }
